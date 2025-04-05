9ea343c0480adf64f0aaa3a8fcecdeccb3c74c66
Rishabh0411 <rishabh.sharma041108@gmail.com>
2025-04-06 00:54:01 +0530

    React_Routers

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,26 +1,19 @@
-// import React, { Component } from 'react';
-// import {render} from 'react-dom';
-
-// export default class App extends Component {
-//     constructor(props) {
-//         super(props);
-//     }
-
-//     render() {
-
-//         return <h1>Testing React</h1>;
-//     }
-// }
-
-// const appDiv = document.getElementById("app");
-// render(<App />, appDiv);
-
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client'; // Import createRoot from react-dom/client
+import Homepage from './Homepage';
+
 
 export default class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
-        return <h1>Testing React</h1>;
+        return (
+            <div>
+                <Homepage />
+            </div>
+        );
     }
 }
 
---
+++ frontend/src/components/Homepage.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+import { BrowserRouter as Router, Route, Routes, Link, Redirect} from "react-router-dom";
+
+export default class Homepage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Router>
+                <Routes>
+                    <Route path="/" element={<p>This is Home page</p>} />
+                    <Route path="/create" element={<CreateRoomPage />} />
+                    <Route path="/join" element={<RoomJoinPage />} />
+                </Routes>
+            </Router>
+        );
+    }
+}
