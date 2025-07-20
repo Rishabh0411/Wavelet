75ceeb7d601820d2a89635b2bf85a302b32ff58d
Rishabh0411 <rishabh.sharma041108@gmail.com>
2025-07-20 19:28:34 +0530

    build

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,22 +1,17 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom/client'; // Import createRoot from react-dom/client
+import ReactDOM from 'react-dom/client';
 import Homepage from './Homepage';
 
-
 export default class App extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div className="center">
-                <Homepage />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="center">
+        <Homepage />
+      </div>
+    );
+  }
 }
 
 const appDiv = document.getElementById("app");
-const root = ReactDOM.createRoot(appDiv); // Create a root
-root.render(<App />); // Use the new render method
+const root = ReactDOM.createRoot(appDiv);
+root.render(<App />);
